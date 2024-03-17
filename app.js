546e3eafd0bf59da4e5b99d86b24a96cbdbcade6
Sayed AG Atif <atif.ghafaar@gmail.com>
2024-03-16 19:47:28 -0500

    Part1 & Part2 are done

--- app.js
+++ app.js
@@ -54,48 +54,47 @@ const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
 const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
 
 
-  //Part 1: Math Problems:
+//Part 1: Math Problems:
 // Here's another example of how this COULD be done,
 // but it SHOULD NOT be done this way. As programmers,
 // we break things into small, manageable pieces so that
 // they can be better understood, scaled, and maintained.
 
 
-//1- Check if all numbers are divisible by 5
+//a- Check if all numbers are divisible by 5
 let divisibleBy5 = (n1 % 5)+(n2 % 5)+(n3 % 5)+(n4 % 5) === 0;
 console.log("All numbers are divisible by 5? " + divisibleBy5);
 
-//2- Check if the first number is larger than the last.
+//b- Check if the first number is larger than the last.
 let firstLarger = n1 > n4;
 console.log("The first number is larger than the last? " + firstLarger);
 
-//3-Accomplish the following arithmetic chain:
-//-Subtract the first number from the second number.
+//c-Accomplish the following arithmetic chain:
+//Subtract the first number from the second number.
 let subTwo = n2 - n1;
 console.log("The result of subtraction is " + subTwo);
-
-//-Multiply the result by the third number.
+//Multiply the result by the third number.
 let multTwo = subTwo * n3;
 console.log("The result of multiplication is " + multTwo);
-
-//-Find the remainder of dividing the result by the fourth number.
+//Find the remainder of dividing the result by the fourth number.
 let remTwo = multTwo % n4;
 console.log("The result of remainder is " + remTwo);
 
-//3-B Arithmetic chain using reassignments:
+//Arithmetic chain using reassignments and template literals:
 let arithmaticChain = n2-n1;
 arithmaticChain = arithmaticChain*n3;
 arithmaticChain = arithmaticChain%n4;
 console.log(`The result of arithmetic chain is ${arithmaticChain}`); // 
 
 
-//4- Change the way that isOver25 calculates so that we do not need to use the NOT operator (!) 
-// in other logic comparisons.
+//d- Change the way that isOver25 calculates so that we do not need to use the NOT operator (!) in other logic comparisons.
 const isNotover25 = ((n1 <= 25) && (n2 <= 25) && (n3 <= 25) && (n4 <= 25));
 console.log(`Is any number less than or equal to 25? ${isNotover25}`); // `     ${isNotover25}` template literals
 
+//----------------------------------------------------------------------------------------------------------
 
-//  Part 2: Practical Math
+
+//Part 2: Practical Math
 /*You are planning a cross-country road trip:
 -The distance of the trip, in total, is 1,500 miles.
 -Your car’s fuel efficiency is as follows:
@@ -113,13 +112,12 @@ console.log(`Is any number less than or equal to 25? ${isNotover25}`); // `     
 let totalDistance = 1500;
 console.log(`The total distance of the trip is ${totalDistance} miles`);
 
-//1- The total gallon of fuel needed for the entire trip are calculated as follows.
+//a- The total gallon of fuel needed for the entire trip are calculated as follows.
 // @ 55 mph = 30 mpg: 
 let at55Mph = 30;
 let totalGallon;
 totalGallon =  totalDistance/ at55Mph;
 console.log(`The total gallon of fuel at 55 mph needed is ${totalGallon} gallons`);
-
 
 // @ 60 mph = 28 mpg:
 let at60Mph = 28;
@@ -131,18 +129,18 @@ let at75Mph = 23;
 totalGallon =  totalDistance/ at75Mph;
 console.log(`The total gallon of fuel at 75 mph needed is ${totalGallon} gallons`);
 
-
-//2- To know whether our budget is enough to cover the fuel cost, first we need to calculate the total fuel cost of the trip. 
+//b- To know whether our budget is enough to cover the fuel cost, first we need to calculate the total fuel cost of the trip. 
 let fuelCost = 175;
 let fuelEfficiency = 3;
 let totalFuelCost = fuelCost * fuelEfficiency;
 console.log(`The total fuel cost of the trip is ${totalFuelCost} $`);
 //Note: As the total fuel cost calculated is 525$, so our budget is not enough to cover the fuel cost and we need 350$ to cover .
 
-//3- Calculate the total time of the trip.
+//c- Calculate the total time of the trip.
 let totalTime = totalDistance / totalFuelCost;
 console.log(`The total time of the trip is ${totalTime} hours`);
 //Note: As the total time calculated is 2.8 hours, so the trip will take 2.8 hours.
+
 
 
 
